a8c7f74be160de34c7bf20b3d96338455a9342a4
Rishav <rishav.ranjan0204@gmail.com>
2023-08-17 00:56:30 +0530

    final commit

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "",// enter your rapid api key here
+    "X-RapidAPI-Key": "5a74ed6fe8msh6ef2c82114bde6ap158dfbjsn87a9030383af",// enter your rapid api key here
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = ""; // enter your key from openweather API
+export const WEATHER_API_KEY = "ba8fbda1e4e5f38b759b50b24cb0aa52"; // enter your key from openweather API
